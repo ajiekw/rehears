49e13d9e355ace8caa23cfadf893bbb727acf5f1
ajiekw <jiekawe@gmail.com>
2020-07-08 22:19:53 +0700

    button added

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,19 @@ class MyApp extends StatelessWidget {
       ],
     ),
   );
+
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'Call'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
     return MaterialApp(
       title: 'flutter layout demo',
       theme: ThemeData(
@@ -53,9 +65,30 @@ class MyApp extends StatelessWidget {
           title: Text('why should i trust you?'),
         ),
         body: Column(
-          children: <Widget>[titleSection],
+          children: <Widget>[titleSection, buttonSection],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
