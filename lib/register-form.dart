c48f1fad92e24621f7e0060868263fdf5e3b28bd
ajiekw <jiekawe@gmail.com>
2020-07-28 10:50:46 +0700

    register

---
+++ lib/register-form.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
+  ));
+}
+
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold is a layout for the major Material Components.
+    return Scaffold();
+  }
+}
