9a696cec86abf8b0378f4ce7c644d4bd16d41379
ajiekw <jiekawe@gmail.com>
2020-07-29 11:20:18 +0700

    register

--- lib/register-form.dart
+++ lib/register-form.dart
@@ -5,9 +5,33 @@ void main() => runApp(Register());
 class Register extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Scaffold is a layout for the major Material Components.
-    return Scaffold(
-      body: Body(),
+    return MaterialApp(
+      home: Scaffold(
+        body: Body(),
+        backgroundColor: Colors.yellow[700],
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 50,
+        horizontal: 110,
+      ),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Text(
+              "Register",
+              style: TextStyle(color: Colors.white, fontSize: 40),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
