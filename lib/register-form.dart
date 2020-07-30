f32dce66c14d891b06252fd11653f432f543e33e
ajiekw <jiekawe@gmail.com>
2020-07-30 09:07:21 +0700

    fixed

--- lib/register-form.dart
+++ lib/register-form.dart
@@ -18,18 +18,130 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(
-        vertical: 50,
-        horizontal: 110,
-      ),
+      padding: const EdgeInsets.only(top: 70.0),
       child: Column(
         children: <Widget>[
           Container(
             child: Text(
               "Register",
-              style: TextStyle(color: Colors.white, fontSize: 40),
+              style: TextStyle(color: Colors.white, fontSize: 20),
             ),
-          )
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(29.5),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 9,
+                      offset: Offset(-4, 6))
+                ]),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Name",
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(29.5),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 9,
+                      offset: Offset(-4, 6))
+                ]),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "E-mails",
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(29.5),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 9,
+                      offset: Offset(-4, 6))
+                ]),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Username",
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(29.5),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 9,
+                      offset: Offset(-4, 6))
+                ]),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Password",
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(29),
+                  child: FlatButton(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                      color: Colors.deepPurple[900],
+                      onPressed: () {},
+                      child: Text(
+                        "Register",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(29),
+                  child: FlatButton(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 35),
+                      color: Colors.deepPurple[900],
+                      onPressed: () {},
+                      child: Text(
+                        "Cancel",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
