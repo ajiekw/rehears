a970f0ee3981db88c422c396bff10d47676ef3b5
ajiekw <jiekawe@gmail.com>
2020-08-17 20:11:54 +0700

    card added

--- lib/home-page.dart
+++ lib/home-page.dart
@@ -18,20 +18,42 @@ class HomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext) {
     return Container(
-      padding: EdgeInsets.only(left: 140, top: 50),
-      child: Column(
-        children: <Widget>[
-          Icon(
-            Icons.face,
-            color: Colors.black,
-            size: 98.0,
-            semanticLabel: "Profile",
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      height: 220,
+      width: double.maxFinite,
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: EdgeInsets.all(7),
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 5),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 1.0),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              "Daily Excercise",
+                              style: TextStyle(
+                                color: Colors.blue[300],
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-          Text(
-            "Home",
-            style: TextStyle(color: Colors.black, fontSize: 36),
-          ),
-        ],
+        ),
       ),
     );
   }
