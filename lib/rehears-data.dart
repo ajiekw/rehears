91fcc5cac7be8b7fce23e1d6966742d9d07eb6df
ajiekw <jiekawe@gmail.com>
2020-08-18 13:23:25 +0700

    Data Added

---
+++ lib/rehears-data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RehearsData {
+  static final Data = [
+    {
+      'name': 'Day One',
+      'value': '30 Exp',
+    },
+    {
+      'name': 'Day Two',
+      'value': '30 Exp',
+    },
+    {
+      'name': 'Day Three',
+      'value': '50 Exp',
+    }
+  ];
+}
