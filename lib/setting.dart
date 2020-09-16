4d5e3e183e20d377c6db4a9761250e7111299f02
ajiekw <jiekawe@gmail.com>
2020-09-17 01:39:33 +0700

    animation added

--- lib/setting.dart
+++ lib/setting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'profile.dart';
 
 void main() => runApp(SettingProfile());
 
@@ -13,7 +14,9 @@ class SettingProfile extends StatelessWidget {
               Icons.arrow_back,
               color: Colors.black,
             ),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(_SettingBody());
+            },
           ),
           title: Text(
             'Setting',
@@ -41,4 +44,22 @@ class SettingBody extends StatelessWidget {
       ),
     );
   }
+}
+
+Route _SettingBody() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => Profile(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(-1, 0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
